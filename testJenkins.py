d9a5b475a3750a5993a854db70556b836fbf5e46
JoeRoss <joseph.ross@leanplum.com>
2018-10-11 11:26:39 -0700

    Argparse Swap for Sys
    the argparse doesn't seem to work properly with jenkins

--- testJenkins.py
+++ testJenkins.py
@@ -1,12 +1,6 @@
-import argparse
+import sys
 
 if __name__ == '__main__':
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--token", help="Credential", type=str)
-  parser.add_argument("--word", help="Random Word", type=str)
-
-  args = parser.parse_args()
-
-  print("Credentials :: " + args.token)
-  print("Word :: " + args.word)
+  print("Credentials :: " + sys.argv[1])
+  print("Word :: " + sys.argv[2])
