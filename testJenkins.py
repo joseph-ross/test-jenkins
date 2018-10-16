7bfda0d40eec701e9b60b09a4ac4f2281fe8c1ef
JoeRoss <joseph.ross@leanplum.com>
2018-10-15 21:24:23 -0700

    Needed to specify everything
    Sigh

--- testJenkins.py
+++ testJenkins.py
@@ -1,13 +1,16 @@
 import sys
 import os
-sys.path.append(os.environ['WORKSPACE'])
-sys.path.append('/Users/joeross/Documents')
-sys.path.append('/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages')
-sys.path.append('/usr/local/bin')
-hold = ['', '/Users/joeross/Documents', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python27.zip', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-darwin', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac/lib-scriptpackages', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-old', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-dynload', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages']
-for item in hold:
-	sys.path.append(item)
-print(sys.path)
+##sys.path.append(os.environ['WORKSPACE'])
+##sys.path.append('/Users/joeross/Documents')
+##sys.path.append('/usr/local/bin')
+##sys.path.append('/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages')
+##sys.path.append('/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/google/cloud')
+##hold = ['', '/Users/joeross/Documents', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python27.zip', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-darwin', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac/lib-scriptpackages', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-old', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-dynload', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages']
+##for item in hold:
+##	sys.path.append(item)
+##print(sys.path)
+sys.path = ['/Users/joeross/Documents/GitHub/test-jenkins', '/Users/joeross/Documents', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python27.zip', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-darwin', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac/lib-scriptpackages', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-old', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-dynload', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages', '/usr/local/bin', '/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages']
+
 
 
 import gcloud
@@ -15,6 +18,7 @@ from google.cloud import datastore
 
 if __name__ == '__main__':
 
+        print("start")
 	print("Credentials :: " + sys.argv[1])
 	print("Word :: " + sys.argv[2])
 
